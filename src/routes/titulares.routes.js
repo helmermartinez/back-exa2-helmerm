25a73a9a93fc11098c0752a3bd55d8f8d5b7218c
helmermartinez <helmermartinez@upeu.edu.pe>
2021-11-20 21:13:51 -0600

    Back Examen2

--- src/routes/titulares.routes.js
+++ src/routes/titulares.routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const titularCtr = require('../controllers/titulares.controller')
 const {checkTokenUser} = require('../auth/token_validation');
 
-router.get('/', checkTokenUser, titularCtr.readAllTitular);
-router.post('/create', checkTokenUser, titularCtr.createTitular);
-router.delete('/delete/:id', checkTokenUser, titularCtr.deleteTitular);
-router.put('/update/:id', checkTokenUser, titularCtr.updateTitular);
+router.get('/',  titularCtr.readAllTitular);
+router.post('/create',  titularCtr.createTitular);
+router.delete('/delete/:id',  titularCtr.deleteTitular);
+router.put('/update/:id',  titularCtr.updateTitular);
 
 module.exports = router;
