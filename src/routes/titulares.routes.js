e71b26b734f1f414c5937b74c8829d562b97f02e
helmermartinez <helmermartinez@upeu.edu.pe>
2021-11-27 19:37:10 -0600

    Final Front v3

--- src/routes/titulares.routes.js
+++ src/routes/titulares.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const titularCtr = require('../controllers/titulares.controller')
 const {checkTokenUser} = require('../auth/token_validation');
 
-router.get('/',  titularCtr.readAllTitular);
+router.get('/', checkTokenUser ,titularCtr.readAllTitular);
 router.post('/create',  titularCtr.createTitular);
 router.delete('/delete/:id',  titularCtr.deleteTitular);
 router.put('/update/:id',  titularCtr.updateTitular);
